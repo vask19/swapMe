51a9e81c59c9ae67149b5dc96a6ea13db2e40cb3
Vasyl Korol mac <gusak.guska223@gmail.com>
2023-01-19 12:05:30 +0100

    project has been created/first commit

---
+++ src/main/java/vask/pet/swapme/swapme/SwapMeApplication.java
@@ -0,0 +1,13 @@
+package vask.pet.swapme.swapme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwapMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vask/pet/swapme/swapme/SwapMeApplicationTests.java
@@ -0,0 +1,13 @@
+package vask.pet.swapme.swapme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
